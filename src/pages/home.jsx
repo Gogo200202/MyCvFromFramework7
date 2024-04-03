0def3f5d0182512fb0b06fe404710fec77944e2b
Gogo200202 <gosheto420@gmail.com>
2024-04-03 09:06:07 +0300

    first commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,176 @@
+import React from 'react';
+import {
+  Page,
+  Navbar,
+  NavLeft,
+  NavTitle,
+  NavTitleLarge,
+  NavRight,
+  Link,
+  Toolbar,
+  Block,
+  BlockTitle,
+  List,
+  ListItem,
+  Button,
+  Panel,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter
+} from 'framework7-react';
+
+
+const HomePage = () => (
+  
+  <Page name="home">
+
+    <Navbar >
+    Georgi Angelov
+      <NavRight >
+        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right" />
+      </NavRight>
+      
+    
+    </Navbar>
+
+  
+
+    {/* Page content */}
+    <BlockTitle>This is my cv build with Framework7.</BlockTitle>
+    <BlockTitle>Here are my some of my projects</BlockTitle>
+    <Block androidTextColor="white">
+      <div className="grid grid-cols-2 grid-gap">
+      
+        <Card outlineMd className="demo-card-header-pic">
+      <CardHeader
+        valign="bottom"
+        style={{
+          backgroundImage: 'url(../img/MusicPLayerApp.png)',
+        }}
+        
+      >
+        
+       <p class="text-color-white" > Music player app</p>
+      </CardHeader>
+      <CardContent>
+        <p className="date">Posted on January 21, 2015</p>
+        <p>
+          Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non
+          felis. Phasellus quis nibh hendrerit...
+        </p>
+      </CardContent>
+      <CardFooter>
+        
+        <Link href="https://github.com/Gogo200202/MusicPlayerApp" external>See in GitHub</Link>
+      </CardFooter>
+    </Card>
+        
+    <Card outlineMd className="demo-card-header-pic">
+      <CardHeader
+        valign="bottom"
+        style={{
+          backgroundImage: 'url(https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg)',
+        }}
+      >
+      <p class="text-color-white" > Journey To Mountains</p>
+      </CardHeader>
+      <CardContent>
+        <p className="date">Posted on January 21, 2015</p>
+        <p>
+          Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non
+          felis. Phasellus quis nibh hendrerit...
+        </p>
+      </CardContent>
+      <CardFooter>
+      
+        <Link>See in GitHub</Link>
+      </CardFooter>
+    </Card>
+    <Card outlineMd className="demo-card-header-pic">
+      <CardHeader
+        valign="bottom"
+        style={{
+          backgroundImage: 'url(https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg)',
+        }}
+      >
+<p class="text-color-white" > Journey To Mountains</p>
+      </CardHeader>
+      <CardContent>
+        <p className="date">Posted on January 21, 2015</p>
+        <p>
+          Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non
+          felis. Phasellus quis nibh hendrerit...
+        </p>
+      </CardContent>
+      <CardFooter>
+       
+        <Link>See in GitHub</Link>
+      </CardFooter>
+    </Card>
+    <Card outlineMd className="demo-card-header-pic">
+      <CardHeader
+        valign="bottom"
+        style={{
+          backgroundImage: 'url(https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg)',
+        }}
+      >
+<p class="text-color-white" > Journey To Mountains</p>
+      </CardHeader>
+      <CardContent>
+        <p className="date">Posted on January 21, 2015</p>
+        <p>
+          Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non
+          felis. Phasellus quis nibh hendrerit...
+        </p>
+      </CardContent>
+      <CardFooter>
+      
+        <Link>See in GitHub</Link>
+      </CardFooter>
+    </Card>
+      </div>
+      </Block>
+
+      
+   {/* 
+    <BlockTitle>Navigation</BlockTitle>
+    <List strong inset dividersIos>
+      <ListItem link="/about/" title="About"/>
+      <ListItem link="/form/" title="Form"/>
+    </List>
+
+    <BlockTitle>Modals</BlockTitle>
+    <Block className="grid grid-cols-2 grid-gap">
+      <Button fill popupOpen="#my-popup">Popup</Button>
+      <Button fill loginScreenOpen="#my-login-screen">Login Screen</Button>
+    </Block>
+
+    <BlockTitle>Panels</BlockTitle>
+    <Block className="grid grid-cols-2 grid-gap">
+      <Button fill panelOpen="left">Left Panel</Button>
+      <Button fill panelOpen="right">Right Panel</Button>
+    </Block>
+
+    <List strong inset dividersIos>
+      <ListItem
+        title="Dynamic (Component) Route"
+        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
+      />
+      <ListItem
+        title="Default Route (404)"
+        link="/load-something-that-doesnt-exist/"
+      />
+      <ListItem
+        title="Request Data & Load"
+        link="/request-and-load/user/123456/"
+      />
+</List>*/}
+
+
+    {/* Toolbar */}
+    <Toolbar bottom>
+    </Toolbar>
+  </Page>
+);
+export default HomePage;
